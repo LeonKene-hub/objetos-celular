e3d16312ec5d3194ae166550cb696c026f7b13ab
Pedro H. Alves <leonkene18@gmail.com>
2023-04-25 10:58:50 -0300

    AAAAAAAAAAAAAAAAA

---
+++ Celular.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace objetos_celular
+{
+    public class Celular
+    {
+        //propriedades 
+        public string Cor = "";
+        public string Modelo = "";
+        public float Tamanho;
+        public bool Ligado;
+
+        //metodos
+        public void Ligar()
+        {
+            Ligado = true;
+        }
+        public void Desligar()
+        {
+            Ligado = false;
+        }
+        public void Fazerligacao()
+        {
+            Console.WriteLine($"Voce nao tem contatos para liga");
+            
+        }
+        public void EnviarMensagem()
+        {
+            Console.WriteLine($"Sem contatos para enviar mensagens");
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using objetos_celular;
+Celular Xperia = new Celular();
+
+Console.WriteLine(@$"
+┌-----------------┐
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│    Precione 1   │
+│    para ligar   │
+├-----------------┤
+│                 │
+│                 │
+└-----------------┘
+");
+
+string opcoes = Console.ReadLine();
+
+if (opcoes == "1")
+{
+    Xperia.Ligar();
+}
+
+if (Xperia.Ligado)
+{
+    switch (opcoes)
+    {
+        case "2":
+        Xperia.Fazerligacao();
+            break;
+        case "3":
+        Xperia.EnviarMensagem();
+            break;
+        default:
+            break;
+    }
+    
+}
+//ligado / desliagado
+switch (Xperia.Ligado)
+{
+    case true:
+
+    do
+    {
+    Console.WriteLine(@$"
+┌-----------------┐
+│                 │
+│2 Fazer ligacao  │
+│                 │
+│3 Enviar mensagem│
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│0 desligar       │
+├-----------------┤
+│                 │
+│                 │
+└-----------------┘
+");
+opcoes = Console.ReadLine();
+        
+    } while (opcoes != "0");
+
+  
+        break;
+    case false:
+    Console.WriteLine(@$"
+┌-----------------┐
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│                 │
+│    Precione 1   │
+│    para ligar   │
+├-----------------┤
+│                 │
+│                 │
+└-----------------┘
+");
+    
+        break;
+    default:
+        break;
+}
